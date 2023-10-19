157ee9787782f7c0a07c97405186a0a208b671f4
biancakendall29 <biancakendall29@gmail.com>
2023-10-19 16:52:09 +0100

    move env

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,14 +5,17 @@ import cors from "cors";
 // import { runCronJob } from "./utils/scheduler";
 // import cron from "node-cron";
 import path from "path";
+const helmet = require("helmet");
 const wordRouter = require("./routes/wordRoutes");
 const todaysWordRouter = require("./routes/todaysWordRoutes");
 
-dotenv.config({ path: "../.env" });
+dotenv.config({ path: "../../app/.env" });
 
 const app = express();
 const port = process.env.PORT || "8000";
 const DB = process.env.DATABASE;
+
+app.use(helmet());
 
 app.use(cors());
 app.use("/api", wordRouter);
